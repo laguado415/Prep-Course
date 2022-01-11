887f2863f2b76ea0d41381e4bc176d7c96f6ada5
laguado415 <jonathanlaguado99@gmail.com>
2022-01-11 13:58:32 -0500

    teminado 3

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -207,7 +207,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-   if(letram === 'a'||letra ==='e'||letra === 'i'||letra === 'o'||letra === 'u')
+   if(letra === 'a'||letra ==='e'||letra === 'i'||letra === 'o'||letra === 'u')
    {return 'Es vocal'}else{return 'Dato incorecto'}
 }
 
