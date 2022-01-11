a1ebb6a61cd995bab7423f79b6066883b0b300ef
laguado415 <jonathanlaguado99@gmail.com>
2022-01-11 00:31:28 -0500

    prueba  commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,16 +7,16 @@ const nuevaString = 'hola';
 const nuevoNum = 48;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,6 +28,7 @@ const nuevoModulo = 21 % 5 === null;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return (srt);
   
 }
 
@@ -35,6 +36,7 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  return (x+y);
   
 }
 
