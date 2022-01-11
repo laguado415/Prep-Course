378ce4d5b1d805f26b0ee5e26b8a2a1742b64e9b
laguado415 <jonathanlaguado99@gmail.com>
2022-01-11 14:49:30 -0500

    21

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -23,7 +23,7 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-  if(status === 1){return 'Onlne';}
+  if(status === 1){return 'Online';}
   if(status === 2){return 'Away';}else{return 'Offline';}
 }
 
