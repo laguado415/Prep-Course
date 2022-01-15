0ae8141c7149eba0b83d0f5b31aafc8f41d931db
laguado415 <jonathanlaguado99@gmail.com>
2022-01-15 15:56:11 -0500

    update partial 11

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -99,6 +99,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return (Math.floor(numero) === numero)? true : false; 
 }
 
 function fizzBuzz(numero) {
