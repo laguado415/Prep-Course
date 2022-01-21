91a1c7e86da822771a8376ec5d34b04677e2e641
laguado415 <jonathanlaguado99@gmail.com>
2022-01-20 22:59:45 -0500

    commit final 2

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -57,8 +57,7 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  let nuevoArray  = {};
-  nuevoArray.push('a');
+  let nuevoArray = [];
   array.map(function(elemento){
     if(elemento[0] === 'a'){nuevoArray.push(elemento)}
   });
